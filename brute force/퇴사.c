9e6e0c480dd63139a8469aabf34ac149be87a1bb
최재영 <52996979+Choi-JY1107@users.noreply.github.com>
2021-08-06 09:05:12 +0900

    퇴사
    https://www.acmicpc.net/problem/14501

---
+++ brute force/퇴사.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int day;
+int ans = 0;
+
+struct{
+	int time;
+	int price;
+} consult[16];
+
+void dp(int today, int now){
+	if(today == day){
+		ans = (ans < now ? now : ans);
+		return;
+	}
+	if(today > day) return;
+	
+	dp(today+1,now);
+	dp(today+consult[today].time, now+consult[today].price);
+}
+
+int main(){
+	scanf("%d",&day);
+	
+	for(int i=0;i<day;i++)
+		scanf("%d %d",&consult[i].time, &consult[i].price);
+		
+	dp(0,0);
+	printf("%d",ans);
+}
