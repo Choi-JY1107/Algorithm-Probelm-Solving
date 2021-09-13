43ff6b5a77644cb4a855c43f81c43ee10383cd1b
최재영 <52996979+Choi-JY1107@users.noreply.github.com>
2021-09-13 15:05:57 +0900

    Update 볼록 껍질.cpp
    Apply ccw algorithm

--- Convex Hull/볼록 껍질.cpp
+++ Convex Hull/볼록 껍질.cpp
@@ -1,93 +1,63 @@
 #include <iostream>
 #include <vector>
-#include <stack>
-#include <cmath>
 #include <algorithm>
-#define PI 3.14159
 using namespace std;
 
 struct coor{
 	int x;
 	int y;
 };
-struct vertex{
-	int index;
-	double angle;
-};
 vector < coor > coors;
-stack < vertex > convex;
-bool flag;
+vector < int > convex;
 
-
-double findAngle(int now){
-	int pre = convex.top().index;
-	return atan2(coors.at(now).x - coors.at(pre).x, coors.at(now).y - coors.at(pre).y);
-}
-bool checkClock(double now){
-	double pre = convex.top().angle;
-	
-	if(pre == 0){
-		if(now > 0 && now != PI) return false;
-		else return true;
-	}
-	else if(pre > 0){
-		if(pre - PI <= now && pre >= now) return true;
-		else return false;
-	}
-	else{
-		if(PI + pre > now && pre < now) return false;
-		else return true;
-	}
+long long ccw(coor a, coor b, coor c) {
+	int x1 = a.x;
+	int y1 = a.y;
+	int x2 = b.x;
+	int y2 = b.y;
+	int x3 = c.x;
+	int y3 = c.y;
+	return (x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1);
 }
 bool init(coor a, coor b){
 	if(a.x != b.x) return a.x < b.x;
 	return a.y < b.y;
 }
 bool compare(coor a, coor b){
-	coor stand = coors.at(0);
-	double angle1 = atan2(a.x - stand.x, a.y - stand.y);
-	double angle2 = atan2(b.x - stand.x, b.y - stand.y);
-	if(angle1 == angle2){
-		if(a.x == b.x) return a.y < b.y;
-		else return a.x > b.x;
-	}
-	return angle1 < angle2;
+	return ccw(coors[0], a, b) > 0;
 }
 
 void findAns(int nowIndex){
-	flag = true;
-	if(nowIndex == coors.size()){
-		nowIndex = 0;
-		flag = false;
-	}
-	double nowAngle = findAngle(nowIndex);
+	if(nowIndex == coors.size()) return;
 	
-	while(convex.size() > 1 && checkClock(nowAngle)){
-		convex.pop();
-		nowAngle = findAngle(nowIndex);
-	}
-	if(flag) {
-		convex.push({nowIndex, nowAngle});
-		findAns(nowIndex+1);
-	}
+	while(convex.size() > 1 && ccw(coors[convex[convex.size()-2]], 
+								   coors[convex[convex.size()-1]],
+								   coors[nowIndex]) <= 0)
+		convex.pop_back();
+		
+	convex.push_back(nowIndex);
+	findAns(nowIndex+1);
+	
 }
 int main() {
-	int n, cnt;
+	int n;
 	coor temp;
-	cin >> n;
 	
+	cin >> n;
 	for(int i=0;i<n;i++){
-		scanf("%d %d",&temp.x, &temp.y);
+		cin >> temp.x >> temp.y;
 		coors.push_back(temp);
 	}
 	
 	sort(coors.begin(), coors.end(), init);
-	convex.push({0,(double)(-100)});
+	convex.push_back(0);
 	sort(coors.begin() + 1, coors.end(), compare);
-	findAns(1);
-	
-	for(cnt=0;!convex.empty();cnt++,convex.pop()) ;
-	cout << cnt;
+	convex.push_back(1);
+	findAns(2);
+	/*
+	for(int i = 0; i<convex.size(); i++)
+		cout << "볼록 껍질 꼭짓점 좌표 : " << coors[convex[i]].x << ", " << coors[convex[i]].y << endl;*/
+	cout << convex.size() << endl;
 	
 	return 0;
 }
